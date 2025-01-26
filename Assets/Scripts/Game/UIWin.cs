bebe8c5b20799792ddf3ac2e5bb7aabe10180e06
isilla <neverovau5@gmail.com>
2025-01-26 11:37:21 +0500

    ASD

---
+++ Assets/Scripts/Game/UIWin.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIWin : MonoBehaviour
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private Image button;
+    [SerializeField] private Sprite normal;
+    [SerializeField] private Sprite pressed;
+    [SerializeField] private TMP_Text title;
+    [SerializeField] private TMP_Text message;
+    
+    private bool _isEnabled;
+    
+    public void ShowWin(Human human)
+    {
+        if(!_isEnabled)
+        {
+            _isEnabled = true;
+            
+            icon.sprite = human.FailIcon;
+            title.text = human.PlayerName;
+            gameObject.SetActive(true);
+            _isEnabled = false;
+        }
+    }
+}
--- Assets/Scripts/Game/UIkill.cs
+++ Assets/Scripts/Game/UIkill.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
@@ -10,9 +11,18 @@ public class UIkill : MonoBehaviour
     [SerializeField] private Sprite normal;
     [SerializeField] private Sprite pressed;
     [SerializeField] private TMP_Text title;
+    [SerializeField] private TMP_Text message;
 
     private bool _isEnabled;
-    
+
+    private List<string> words = new()
+    {
+        "Чувак, эта вечеринка полный отстой.",
+        "Да я просто в носу ковырял, алё, чо за агр?",
+        "Лаги! Дедовский сервак не тянет мою мощь.",
+        "Я не проиграл, я просто элегантно ретировался!",
+    };
+
     public void ShowKill(Human human)
     {
         if(!_isEnabled)
@@ -21,6 +31,7 @@ public class UIkill : MonoBehaviour
             
             icon.sprite = human.FailIcon;
             title.text = human.PlayerName;
+            message.text = words[Random.Range(0, words.Count)];
             gameObject.SetActive(true);
             DOTween.Sequence().AppendInterval(5f)
                 .AppendCallback(() =>
@@ -36,6 +47,7 @@ public class UIkill : MonoBehaviour
                 .OnComplete(() =>
                 {
                     _isEnabled = false;
+                    gameObject.SetActive(false);
                 });
         }
     }
